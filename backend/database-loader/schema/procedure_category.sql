2e5a848418110e4483ea5b05d2bac5b9c30bbf69
pelicanactor <arnaud.caru@gmail.com>
2025-10-23 17:03:36 +0200

    Create procedure_category.sql

---
+++ backend/database-loader/schema/procedure_category.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS procedure_category;
+CREATE TABLE IF NOT EXISTS procedure_category (
+    id TEXT PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    slug TEXT UNIQUE NOT NULL,
+    emoji TEXT UNIQUE NOT NULL,
+    description TEXT
+);
