72885128168f693940026d1b804fb2f34f97efca
pelicanactor <arnaud.caru@gmail.com>
2025-11-12 23:04:05 +0100

    add more MRI procedure

--- backend/mario-health-data-pipeline/models/staging/united_healthcare/pp1_00_plan/united_pp1_00_rates_top_cpt.sql
+++ backend/mario-health-data-pipeline/models/staging/united_healthcare/pp1_00_plan/united_pp1_00_rates_top_cpt.sql
@@ -5,16 +5,16 @@
 }}
 
 WITH t0 AS (
-    SELECT DISTINCT code
+    SELECT DISTINCT code, code_type
     FROM {{ ref('procedure_billing_code') }}
-    WHERE code_type = 'CPT'
 )
 
 SELECT
   rates.*
 FROM t0
-LEFT JOIN {{ source('mario-mrf-data', 'uhc_apple_in_network_rates') }} AS rates
+JOIN {{ source('mario-mrf-data', 'uhc_apple_in_network_rates') }} AS rates -- don't do a left join here otherwise you
+-- have null rates if the insurer doesn't list some codes
 ON
   t0.code = rates.billing_code
-  AND rates.billing_code_type = 'CPT'
+  AND t0.code_type = rates.billing_code_type
   AND rates.billing_code_type_version = 2025
