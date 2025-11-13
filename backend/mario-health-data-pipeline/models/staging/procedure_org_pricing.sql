2ee909a3c7f489b9078b19cfd31a56e69e3a7b9f
pelicanactor <arnaud.caru@gmail.com>
2025-11-13 15:13:16 +0100

    Create procedure_org_pricing.sql

---
+++ backend/mario-health-data-pipeline/models/staging/procedure_org_pricing.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+SELECT
+    CONCAT(org_id, '_', procedure_id, '_', carrier_id) AS id,
+    org_id,
+    procedure_id,
+    carrier_id,
+    carrier_name,
+    COUNT(DISTINCT provider_id) AS count_provider,
+    MIN(price) AS min_price,
+    MAX(price) AS max_price,
+    AVG(price) AS avg_price,
+FROM
+    {{ ref('procedure_pricing') }}
+GROUP BY
+    1,2,3,4,5
