19134acd4af1fa53f2e147e6515150cc2068a7ca
armanzaman <az@armanzaman.com>
2025-12-24 03:26:21 +0800

    chore: remove orphaned catch-all route directories causing build errors

--- frontend/src/app/procedures/[...slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-import ProcedureDetailClient from './ProcedureDetailClient';
-
-// Force dynamic rendering - no static generation for procedure detail pages
-export const dynamic = "force-dynamic";
-
-export default function ProcedureDetailPage() {
-    return <ProcedureDetailClient />;
-}
