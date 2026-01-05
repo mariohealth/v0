7c89308cffe1cccd6693e23e82ebad54be8fa47a
pelicanactor <arnaud.caru@gmail.com>
2026-01-05 16:38:19 +0100

    do the refactorisation

--- backend/mario-health-data-pipeline/models/staging/code_pricing.sql
+++ backend/mario-health-data-pipeline/models/staging/code_pricing.sql
@@ -3,35 +3,81 @@
     materialized='table'
   )
 }}
+-- This table has one row per hospital per NPI type 1 per billing code per carrier plan.
+-- This is where we combine different plans and we combine institutional with professional rates.
+WITH t_prof AS (
+    SELECT
+        hospital_id,
+        npi,
+        healthcare_provider_taxonomy_code,
+        billing_code,
+        billing_code_type,
+        billing_code_type_version,
+        professional_rate,
+        '1' AS carrier_id,
+        '2' AS carrier_plan_id,
+    FROM
+        {{ ref('cigna_national_oap_rates_prof_facility') }}
+    UNION ALL BY NAME
+    SELECT
+        hospital_id,
+        npi,
+        healthcare_provider_taxonomy_code,
+        billing_code,
+        billing_code_type,
+        billing_code_type_version,
+        professional_rate,
+        '2' AS carrier_id,
+        '1' AS carrier_plan_id,
+    FROM
+        {{ ref('united_pp1_00_rates_prof_facility') }}
+),
+
+t_inst AS (
+    SELECT
+        hospital_id,
+        billing_code,
+        billing_code_type,
+        billing_code_type_version,
+        institutional_rate,
+        '1' AS carrier_id,
+        '2' AS carrier_plan_id,
+    FROM
+        {{ ref('cigna_national_oap_rates_inst_facility') }}
+    UNION ALL BY NAME
+    SELECT
+        hospital_id,
+        billing_code,
+        billing_code_type,
+        billing_code_type_version,
+        institutional_rate,
+        '2' AS carrier_id,
+        '1' AS carrier_plan_id,
+    FROM
+        {{ ref('united_pp1_00_rates_inst_facility') }}
+)
+
 
 SELECT
-    hospital_id,
-    billing_code,
-    billing_code_type,
-    billing_code_type_version,
-    npi,
-    healthcare_provider_taxonomy_code,
-    professional_rate,
-    institutional_rate,
-    total_rate,
-    '2' AS carrier_id,
-    '1' AS carrier_plan_id,
+    COALESCE(t_prof.hospital_id, t_inst.hospital_id) AS hospital_id,
+    COALESCE(t_prof.billing_code, t_inst.billing_code) AS billing_code,
+    COALESCE(t_prof.billing_code_type, t_inst.billing_code_type) AS billing_code_type,
+    COALESCE(t_prof.billing_code_type_version, t_inst.billing_code_type_version) AS billing_code_type_version,
+    t_prof.npi,
+    t_prof.healthcare_provider_taxonomy_code,
+    t_prof.professional_rate,
+    t_inst.institutional_rate,
+    COALESCE(t_prof.professional_rate, 0) + COALESCE(t_inst.institutional_rate, 0) AS total_rate,
+    COALESCE(t_prof.carrier_id, t_inst.carrier_id) AS carrier_id,
+    COALESCE(t_prof.carrier_plan_id, t_inst.carrier_plan_id) AS carrier_plan_id,
 FROM
-    {{ ref('united_pp1_00_rates_total_facility') }}
-
--- doesn't exist yet
-
---UNION ALL
---SELECT
---hospital_id,
---    billing_code,
---    billing_code_type,
---    billing_code_type_version,
---    npi,
---    healthcare_provider_taxonomy_code,
---    professional_rate,
---    institutional_rate,
---    total_rate,
---'1' AS carrier_id,
---    '2' AS carrier_plan_id,
---FROM
+    t_prof
+FULL OUTER JOIN
+    t_inst
+ON
+    t_prof.hospital_id = t_inst.hospital_id
+    AND t_prof.billing_code = t_inst.billing_code
+    AND t_prof.billing_code_type = t_inst.billing_code_type
+    AND t_prof.billing_code_type_version = t_inst.billing_code_type_version
+    AND t_prof.carrier_id = t_inst.carrier_id
+    AND t_prof.carrier_plan_id = t_inst.carrier_plan_id
--- backend/mario-health-data-pipeline/models/staging/procedure_pricing.sql
+++ backend/mario-health-data-pipeline/models/staging/procedure_pricing.sql
@@ -3,56 +3,66 @@
     materialized='table'
   )
 }}
- -- this is the model where we will union pricing from all different carriers and plans
+ -- This is the model where we go from billing codes to Mario-defined procedures to make it more user friendly.
+ -- Because a procedure is made up of multiple CPT codes, we take the rate of the cheapest CPT code.
 
-WITH t_union_all AS (
+WITH t0 AS (
+    SELECT
+        map.procedure_id,
+        rates.hospital_id,
+        rates.npi,
+        rates.carrier_id,
+        rates.carrier_plan_id,
+        ROUND(MIN(professional_rate), 0) AS professional_rate_min,
+        ROUND(MIN(institutional_rate), 0) AS institutional_rate_min,
+        ROUND(MIN(total_rate), 0) AS total_rate_min,
+    FROM
+       {{ ref('code_pricing') }} AS rates
+    JOIN
+        {{ ref('procedure_billing_code') }} AS map
+ON
+    rates.billing_code = map.code
+    AND rates.billing_code_type = map.code_type
+    AND CAST(rates.billing_code_type_version AS STRING) = '2025'
+GROUP BY
+    map.procedure_id,
+    rates.npi,
+    rates.hospital_id,
+    rates.carrier_id,
+    rates.carrier_plan_id
+),
 
-SELECT
-    id,
-    procedure_id,
-    org_id,
-    provider_id,
-    provider_location_id,
-    carrier_id,
-    carrier_name,
-    NULL AS professional_rate_min,
-    NULL AS institutional_rate_min,
-    NULL AS total_rate_min,
-    price,
-FROM
-    {{ ref('cigna_national_oap_proc_pricing') }}
-
-UNION ALL
-
-SELECT
-    id,
-    procedure_id,
-    org_id,
-    provider_id,
-    provider_location_id,
-    carrier_id,
-    carrier_name,
-    professional_rate_min,
-    institutional_rate_min,
-    total_rate_min,
-    total_rate_min AS price,
-FROM
-    {{ ref('united_pp1_00_proc_pricing') }}
-
+t1 AS (
+    SELECT
+        CONCAT(procedure_id, '_', hospital_id, '_', npi, '_', 'united_pp1_00') AS id,
+        procedure_id,
+        hospital_id AS org_id,
+        npi AS provider_id,
+        CONCAT(hospital_id, "_", npi) AS provider_location_id,
+        carrier_id,
+        carrier_name,
+        {{ round_price('professional_rate_min') }} AS professional_rate_min,
+        {{ round_price('institutional_rate_min') }} AS institutional_rate_min,
+        {{ round_price('total_rate_min') }} AS total_rate_min,
+        {{ round_price('total_rate_min') }} AS price, -- this is left here to not brake the back end but will have to be
+--        removed at some point.
+    FROM
+        t0
 )
 
+
 SELECT
-    t_union_all.id,
-    t_union_all.procedure_id,
-    t_union_all.org_id,
-    t_union_all.provider_id,
-    t_union_all.provider_location_id,
-    t_union_all.carrier_id,
-    t_union_all.carrier_name,
-    t_union_all.professional_rate_min,
-    t_union_all.institutional_rate_min,
-    t_union_all.total_rate_min,
-    t_union_all.price,
+    t1.id,
+    t1.procedure_id,
+    t1.org_id,
+    t1.provider_id,
+    t1.provider_location_id,
+    t1.carrier_id,
+    t1.carrier_name,
+    t1.professional_rate_min,
+    t1.institutional_rate_min,
+    t1.total_rate_min,
+    t1.price,
     CONCAT(
               t_npi.provider_name_prefix_text,
               " ",
@@ -67,8 +77,8 @@ SELECT
         t_npi.provider_credential_text
         ) AS provider_name,
 FROM
-    t_union_all
+    t1
 JOIN
       {{ source('mario-mrf-data', 'npidata_pfile_20050523-20250907') }} AS t_npi
     ON
-        t_union_all.provider_id = t_npi.npi
+        t1.provider_id = t_npi.npi
