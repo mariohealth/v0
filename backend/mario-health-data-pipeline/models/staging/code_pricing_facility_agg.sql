2ba6b83c3f25b1dededef84c587bd19aeafbd57f
pelicanactor <arnaud.caru@gmail.com>
2026-01-15 10:50:33 +0100

    Update code_pricing_facility_agg.sql

--- backend/mario-health-data-pipeline/models/staging/code_pricing_facility_agg.sql
+++ backend/mario-health-data-pipeline/models/staging/code_pricing_facility_agg.sql
@@ -15,9 +15,18 @@ SELECT
     billing_code_type_version,
     COUNT(*) AS count_rows,
     COUNT(DISTINCT npi) AS count_distinct_npi,
-    MIN(professional_rate) AS min_professional_rate,
-    AVG(professional_rate) AS avg_professional_rate,
-    MAX(professional_rate) AS max_professional_rate,
+
+    MIN(rate_professional) AS min_professional_rate, -- see the code_pricing model for an explanation of why so many rates
+    AVG(rate_professional) AS avg_professional_rate,
+    MAX(rate_professional) AS max_professional_rate,
+
+    MIN(rate_technical) AS min_technical_rate,
+    MAX(rate_technical) AS max_technical_rate,
+    ROUND(AVG(rate_technical)) AS avg_technical_rate,
+
+    MIN(rate_global) AS min_global_rate,
+    MAX(rate_global) AS max_global_rate,
+    ROUND(AVG(rate_global)) AS avg_global_rate,
 
     MIN(institutional_rate) AS min_institutional_rate,
     AVG(institutional_rate) AS avg_institutional_rate,
