7fdc813b6767e87b9706d530e06808588e86bc74
pelicanactor <arnaud.caru@gmail.com>
2026-01-16 17:11:34 +0100

    Create fact_drug_prices.sql

---
+++ backend/mario-health-data-pipeline/models/facts/fact_drug_prices.sql
@@ -0,0 +1,49 @@
+{{ config(materialized='table') }}
+
+WITH prices AS (
+
+  SELECT
+    raw_drug_name,
+    drug_form,
+    price,
+    quantity,
+    pharmacy_id,
+    source_id,
+    product_url,
+  FROM {{ ref('stg_costplus_prices') }}
+
+  UNION ALL
+
+  SELECT
+    raw_drug_name,
+    drug_form,
+    price,
+    quantity,
+    pharmacy_id,
+    source_id,
+    product_url,
+  FROM {{ ref('stg_goodrx_prices') }}
+
+),
+
+matched AS (
+
+  SELECT
+    p.*,
+    m.rxnorm_cui
+  FROM prices p
+  LEFT JOIN {{ ref('drug_match_rules') }} m
+    ON p.raw_drug_name = m.raw_drug_name
+
+)
+
+SELECT
+  rxnorm_cui,
+  pharmacy_id,
+  source_id,
+  price,
+  quantity,
+  product_url,
+  last_updated
+FROM matched
+WHERE rxnorm_cui IS NOT NULL
