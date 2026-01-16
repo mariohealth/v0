858d37a86cb69686819f08dfe3ce7e5025f9319f
pelicanactor <arnaud.caru@gmail.com>
2026-01-16 15:53:14 +0100

    Create stg_rxnorm_conso.sql

---
+++ backend/mario-health-data-pipeline/models/staging/rxnorm/stg_rxnorm_conso.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='table') }}
+
+SELECT
+  CAST(rxcui AS INT64) AS rxnorm_cui,
+  str AS drug_name,
+  tty,
+  sab
+FROM {{ ref('rxnconso') }}
+WHERE sab = 'RXNORM'
+  AND suppress = 'N'
+  AND tty IN ('IN', 'SCD', 'SBD')
