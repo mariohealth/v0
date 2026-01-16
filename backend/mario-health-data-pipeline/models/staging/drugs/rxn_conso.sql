d9a34cead1b036daa72d0ba9cb2806782e4b1218
pelicanactor <arnaud.caru@gmail.com>
2026-01-16 14:21:53 +0100

    Update rxn_conso.sql

--- backend/mario-health-data-pipeline/models/staging/drugs/rxn_conso.sql
+++ backend/mario-health-data-pipeline/models/staging/drugs/rxn_conso.sql
@@ -7,13 +7,15 @@
 SELECT
     rxcui,
 --    term_language, -- removing because it's always 'ENG'
-    atom_unique_id,
-    source_asserted_atom_id,
-    source_asserted_concept_id,
-    source_abbreviation,
-    source_term_type,
-    code,
-    string,
+    rxaui, -- Unique identifier for atom (RxNorm Atom ID)
+    saui, -- Source asserted atom identifier [optional]
+    scui, -- Source asserted concept identifier [optional]
+    sdui, -- Source asserted descriptor identifier [optional] -- all NULLs as of Jan 2026
+    sab, -- Source abbreviation
+    tty, -- Term type in source
+    code, -- "Most useful" source asserted identifier (if the source vocabulary has more than one identifier), or a
+--    RxNorm-generated source entry identifier (if the source vocabulary has none.)
+    str, -- String
 --    suppress, -- removing because it's always 'N'
 --    content_view_flag, -- removing because it's always '4096'
 FROM
