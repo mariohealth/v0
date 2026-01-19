64eb8597a3c70091a7b1f8758fc230545122a9ac
pelicanactor <arnaud.caru@gmail.com>
2026-01-19 16:27:13 +0100

    Update fact_drug_prices.sql

--- backend/mario-health-data-pipeline/models/facts/fact_drug_prices.sql
+++ backend/mario-health-data-pipeline/models/facts/fact_drug_prices.sql
@@ -2,40 +2,52 @@
 
 WITH t_costplus AS (
     SELECT
-        raw_drug_name,
-        drug_form,
-        price,
-        quantity,
-        pharmacy_id,
-        source_id,
-        product_url,
-    FROM {{ ref('stg_costplus_prices') }}
-    JOIN
-
+        cpd_match.rxnorm_cui,
+        cpd_p.price,
+        cpd_p.quantity,
+        cpd_p.pharmacy_id,
+        cpd_p.source_id,
+        cpd_p.product_url,
+    FROM {{ ref('drug_match_rules_costplus') }} AS cpd_match
+    JOIN {{ ref('stg_costplus_prices') }} AS cpd_p
+    ON cpd_p.product_url = cpd_match.product_url -- can join only on URL for Cost Plus because they are unique, unlike
+--    for our GoodRX data
 ),
 
 t_goodrx AS (
     SELECT
-        raw_drug_name,
-        drug_form,
+        grx_match.rxnorm_cui,
+        grx_p.price,
+        grx_p.quantity,
+        grx_p.pharmacy_id,
+        grx_p.source_id,
+        grx_p.product_url,
+    FROM {{ ref('drug_match_rules_goodrx') }} AS grx_match
+    JOIN {{ ref('stg_goodrx_prices') }} AS grx_p
+    ON grx_match.product_url = grx_p.product_url
+),
+
+prices AS (
+    SELECT
+        rxnorm_cui,
         price,
         quantity,
         pharmacy_id,
         source_id,
         product_url,
-    FROM {{ ref('stg_goodrx_prices') }}
-    JOIN
-
-)
-
-prices AS (
-
-
+    FROM
+        t_costplus
 
   UNION ALL
-
-
-
+    SELECT
+        rxnorm_cui,
+        price,
+        quantity,
+        pharmacy_id,
+        source_id,
+        product_url,
+    FROM
+        t_goodrx
 )
 
 SELECT
@@ -45,5 +57,4 @@ SELECT
   price,
   quantity,
   product_url,
-FROM matched
-WHERE rxnorm_cui IS NOT NULL
+FROM prices
