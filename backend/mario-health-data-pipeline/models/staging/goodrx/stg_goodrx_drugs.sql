72b0c1cbc5794e7a0dc78a09dc898f81ef587272
pelicanactor <arnaud.caru@gmail.com>
2026-01-21 15:31:21 +0100

    Update stg_goodrx_drugs.sql

--- backend/mario-health-data-pipeline/models/staging/goodrx/stg_goodrx_drugs.sql
+++ backend/mario-health-data-pipeline/models/staging/goodrx/stg_goodrx_drugs.sql
@@ -16,7 +16,10 @@ SELECT
   drug_full_name,
   primary_name,
   secondary_name,
-  CASE WHEN primary_name LIKE '%generic%' OR secondary_name LIKE '%generic%' THEN 'yes' ELSE 'unknown' END AS is_generic,
+  CASE WHEN primary_name LIKE '%generic%' THEN 'false'
+    WHEN secondary_name LIKE '%generic%' THEN 'true'
+    WHEN primary_name NOT LIKE '%generic%' AND secondary_name NOT LIKE '%generic%' THEN 'false'
+    ELSE 'unknown' END AS is_generic,
   TRIM(SPLIT(drug_full_name, '(')[SAFE_OFFSET(0)]) AS drug_name,
   TRIM(REPLACE(SPLIT(drug_full_name, '(')[SAFE_OFFSET(1)], ')','')) AS quantity,
   dosage,
