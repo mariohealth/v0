c6cc73313e308ebb9a09a8fed565c3b436acfebf
pelicanactor <arnaud.caru@gmail.com>
2026-01-23 12:06:29 +0100

    Update fact_drug_prices.sql

--- backend/mario-health-data-pipeline/models/facts/fact_drug_prices.sql
+++ backend/mario-health-data-pipeline/models/facts/fact_drug_prices.sql
@@ -2,7 +2,7 @@
 
 WITH t_costplus AS (
     SELECT
-        cpd_match.rxnorm_cui,
+        cpd_match.rxcui_scd,
         cpd_p.price,
         cpd_p.quantity,
         cpd_p.pharmacy_id,
@@ -12,12 +12,12 @@ WITH t_costplus AS (
     JOIN {{ ref('stg_costplus_prices') }} AS cpd_p
     ON cpd_p.product_url = cpd_match.product_url -- can join only on URL for Cost Plus because they are unique, unlike
 --    for our GoodRX data
-    AND cpd_match.needs_review IS FALSE
+
 ),
 
 t_goodrx AS (
     SELECT
-        grx_match.rxnorm_cui,
+        grx_match.rxcui_scd,
         grx_p.price,
         grx_p.quantity,
         grx_p.pharmacy_id,
@@ -25,15 +25,14 @@ t_goodrx AS (
         grx_p.product_url,
     FROM {{ ref('stg_goodrx_drugs_matched') }} AS grx_match
     JOIN {{ ref('stg_goodrx_prices') }} AS grx_p
-    ON grx_match.product_url = grx_p.product_url -- The URLs aren't unique in our GoodRX data so we can't join just on them:
-    AND grx_match.raw_drug_name = grx_p.raw_drug_name
-    AND grx_match.drug_form = grx_p.drug_form
-    AND grx_match.needs_review IS FALSE
+    ON grx_match.url_param = REPLACE(grx_p.product_url, 'https://www.goodrx.com/', '') -- The URLs aren't unique in our GoodRX data so we can't join just on them:
+    AND grx_match.dosage = grx_p.drug_form
+
 ),
 
 prices AS (
     SELECT
-        rxnorm_cui,
+        rxcui_scd,
         price,
         quantity,
         pharmacy_id,
@@ -44,7 +43,7 @@ prices AS (
 
   UNION ALL
     SELECT
-        rxnorm_cui,
+        rxcui_scd,
         price,
         quantity,
         pharmacy_id,
@@ -55,7 +54,7 @@ prices AS (
 )
 
 SELECT
-  rxnorm_cui,
+  rxcui_scd,
   pharmacy_id,
   source_id,
   price,
