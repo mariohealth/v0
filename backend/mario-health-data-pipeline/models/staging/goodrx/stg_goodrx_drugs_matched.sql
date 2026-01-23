350de4eb9b069abe5f1df04dca36361abcea5041
pelicanactor <arnaud.caru@gmail.com>
2026-01-23 11:55:50 +0100

    update goodrx similarity seed

--- backend/mario-health-data-pipeline/models/staging/goodrx/stg_goodrx_drugs_matched.sql
+++ backend/mario-health-data-pipeline/models/staging/goodrx/stg_goodrx_drugs_matched.sql
@@ -1,2 +1,11 @@
 {{ config(materialized='table') }}
 
+SELECT -- For Good RX the combination of url_param and dosage is unique so we use this combination as an "id".
+    url_param,
+    dosage,
+    CASE WHEN best_scd_similarity >= 0.9 THEN best_scd_rxcui ELSE NULL END AS rxcui_scd,
+    CASE WHEN best_sbd_similarity >= 0.9 THEN best_sbd_rxcui ELSE NULL END AS rxcui_sbd
+FROM
+    {{ ref('drug_match_rules_goodrx') }}
+WHERE
+    best_scd_similarity >= 0.9
