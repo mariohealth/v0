242ccce4fd718efc2f44696da4b6bfbb5f29f05e
pelicanactor <arnaud.caru@gmail.com>
2026-02-11 17:05:33 +0100

    Update facilities_addresses.sql

--- backend/mario-health-data-pipeline/models/staging/nppes/facilities_addresses.sql
+++ backend/mario-health-data-pipeline/models/staging/nppes/facilities_addresses.sql
@@ -25,30 +25,10 @@ WITH t0 AS (
         AND provider_business_practice_location_address_state_name NOT IN ('AA', 'AE', 'AP', 'AS', 'GU', 'MP', 'PR', 'VI')
         AND healthcare_provider_taxonomy_group_1 = ''
         AND healthcare_provider_primary_taxonomy_switch_1 = 'Y'
-        AND healthcare_provider_taxonomy_code_1 IN (
-          "282N00000X",
-          "283Q00000X",
-          "283X00000X",
-          "281P00000X",
-          "282E00000X",
-          "286500000X",
-          "284300000X",
-          "261QA1903X",
-          "261QC1500X",
-          "261QF0400X",
-          "261QM0801X",
-          "261QR0200X",
-          "261QP2300X",
-          "261QU0200X",
-          "261Q00000X",
-          "261QR0206X",
-          "261QR0207X",
-          "261QR0208X",
-          "291U00000X",
-          "261QX0100X",
-          "261QP2000X",
-          "261QP3300X",
-          "261QR0400X"
+        AND LEFT(healthcare_provider_taxonomy_code_1, 2) IN (
+        "26", -- Ambulatory Health Care Facilities
+        "28", -- Hospitals
+        "29" -- Laboratories
         )
 
 ),
